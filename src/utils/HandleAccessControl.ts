0620b2dcbf47ebe8031e520f18b9f369f40e11d0
vcarvalho0 <vitor1.santos00@gmail.com>
2025-05-25 20:46:46 -0300

    feat: add product and company ownership verification methods

--- src/utils/HandleAccessControl.ts
+++ src/utils/HandleAccessControl.ts
@@ -5,6 +5,55 @@ import { Role, User } from '@prisma/client';
 class HandleAccessControl {
   constructor(private readonly prisma: PrismaService) {}
 
+  async verifyProductOwnership(userPayload: Partial<User>, productId: number): Promise<void> {
+    await this.verifyCompanyOwnership(userPayload);
+
+    const company = await this.prisma.company.findUnique({
+      where: {
+        userId: userPayload.id,
+      },
+    });
+
+    if (!company) {
+      throw new NotFoundException('Empresa não encontrada');
+    }
+
+    const products = await this.prisma.product.findUnique({
+      where: {
+        companyId: company.id,
+        id: productId,
+      },
+    });
+
+    if (!products) {
+      throw new NotFoundException('Produto não encontrado ou não pertence a empresa');
+    }
+  }
+
+  async verifyCompanyOwnership(payload: Partial<User>): Promise<void> {
+    const { role, id } = payload;
+
+    if (role !== Role.CompanyOwner) {
+      throw new ForbiddenException('Acesso não autorizado.');
+    }
+
+    const owner = await this.prisma.user.findUnique({
+      where: {
+        id,
+        company: {
+          userId: id,
+        },
+      },
+      include: {
+        company: true,
+      },
+    });
+
+    if (!owner) {
+      throw new NotFoundException('Owner não encontrado!');
+    }
+  }
+
   verifyAdminRole(payload: Partial<User>): void {
     const { role } = payload;
 
